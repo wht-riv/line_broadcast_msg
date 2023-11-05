526dc7e478ee334cb950fa7548f40f06fb4f6446
hjmr <2620990+hjmr@users.noreply.github.com>
2023-11-05 21:32:55 +0900

    modified for V3 API.

--- broadcast_msg.py
+++ broadcast_msg.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, BroadcastRequest, TextMessage
+
 
 channel_access_token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 
@@ -10,9 +10,14 @@ if channel_access_token is None:
     print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
     sys.exit(1)
 
-line_bot_api = LineBotApi(channel_access_token)
-line_bot_api.broadcast(
-    [
-        TextSendMessage(text="THIS IS A BROADCAST MESSAGE"),
-    ]
-)
+configuration = Configuration(access_token=channel_access_token)
+
+with ApiClient(configuration) as api_client:
+    line_bot_api = MessagingApi(api_client)
+    line_bot_api.broadcast(
+        BroadcastRequest(
+            messages=[
+                TextMessage(text="THIS IS A BROADCAST MESSAGE")
+            ]
+        )
+    )
